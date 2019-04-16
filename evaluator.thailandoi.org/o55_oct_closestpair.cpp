2c8a5c9b29d32830abf70e02d0c13fbbd023641b
Autoratch <autorockza@gmail.com>
2019-04-16 11:41:52 +0700

    God sent <sweep line> to me!

--- evaluator.thailandoi.org/o55_oct_closestpair.cpp
+++ evaluator.thailandoi.org/o55_oct_closestpair.cpp
@@ -1,69 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define point pair<int,int>
-#define x first
-#define y second
- 
-const int N = 1e5;
- 
-point s[N],sx[N],sy[N];
+
 int n;
- 
-bool cmpx(const point &a,const point &b){ if(a.x!=b.x) return a.x<b.x; else return a.y<b.y; }
- 
-bool cmpy(const point &a,const point &b){ if(a.y!=b.y) return a.y<b.y; else return a.x<b.x; }
- 
-long long dist(point a,point b){ return (a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y); }
- 
-long long solvestrip(point strip[],int sz,long long mn)
-{
-    for(int i = 0;i < sz;i++) for(int j = i+1;j < sz and strip[j].y-strip[i].y<mn;j++) mn = min(mn,dist(strip[i],strip[j]));
-    return mn;
-}
- 
-long long solve(point px[],point py[],int sz)
-{
-    if(sz<=3)
-    {
-        long long mn = LLONG_MAX;
-        for(int i = 0;i < sz;i++) for(int j = i+1;j < sz;j++) mn = min(mn,dist(px[i],px[j]));
-        return mn;
-    }
- 
-    int m = sz/2;
-    int li = 0,ri = 0;
-    point mid = px[m];
-    point pyl[m+1];
-    point pyr[n-m+1];
- 
-    for(int i = 0;i < n;i++)
-    {
-        if(py[i].x<=mid.x) pyl[li++] = py[i];
-        else pyr[ri++] = py[i];
-    }
- 
-    long long mnl = solve(px,pyl,m);
-    long long mnr = solve(px+m,pyr,sz-m);
-    long long mn = min(mnl,mnr);
- 
-    point strip[sz];
-    int j = 0;
- 
-    for(int i = 0;i < sz;i++) if(abs(py[i].x-mid.x)<mn){ strip[j] = py[i]; j++; }
- 
-    return min(mn,solvestrip(strip,j,mn));
-}
- 
+vector<pair<int,int> > pts;
+set<pair<int,int> > s;
+long long ans = 1e18;
+
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0);
- 
+
     cin >> n;
- 
-    for(int i = 0;i < n;i++){ cin >> s[i].x >> s[i].y; sx[i] = sy[i] = s[i]; }
- 
-    sort(sx,sx+n,cmpx);
-    sort(sy,sy+n,cmpy);
- 
-    cout << solve(sx,sy,n);
+
+    pts.resize(n);
+    for(int i = 0;i < n;i++) cin >> pts[i].first >> pts[i].second;
+    sort(pts.begin(),pts.end());
+
+    int j = 0;
+    for(int i = 0;i < n;i++)
+    {
+        int d = ceil(sqrt(ans));
+        while(pts[i].first-pts[j].first>=ans)
+        {
+            s.erase({pts[j].second,pts[j].first});
+            j++;
+        }
+        auto it1 = s.lower_bound({pts[i].second-d,pts[i].first});
+        auto it2 = s.upper_bound({pts[i].second+d,pts[i].first});
+        for(auto it = it1;it != it2;it++)
+        {
+            int dx = pts[i].first-it->second;
+            int dy = pts[i].second-it->first;
+            ans = min(ans,1LL*dx*dx+1LL*dy*dy);
+        }
+        s.insert({pts[i].second,pts[i].first});
+    }
+
+    cout << ans;
 }
