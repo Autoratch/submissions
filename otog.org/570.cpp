13558ba53c605833e8c5a935c5a6e7d2b4c162e6
Autoratch <autorockza@gmail.com>
2019-05-29 13:24:28 +0700

    Math

---
+++ otog.org/570.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t,a,b,c;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> a >> b >> c;
+        cout << -b/2/a << '\n';
+    }
+}
