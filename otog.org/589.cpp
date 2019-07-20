961507faf92e2ad8039f1d11a2433ddf6c3bf045
Autoratch <autorockza@gmail.com>
2019-07-20 20:36:35 +0700

    +-*/

---
+++ otog.org/589.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int my_mul(int a,int b){ return a*b; }
+
+int my_div(int a,int b){ return a/b; }
+
+int my_add(int a,int b){ return a+b; }
+
+int my_sub(int a,int b){ return a-b; }
