6b965a5d84651e05a9a63eeee80b289ab7d19483
Krittitee Ratchanan <autorockza@gmail.com>
2019-09-04 09:42:18 +0700

    Add files via upload

---
+++ otog.org/645.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int a;
+    char b;
+
+    cin >> a >> b;
+
+    cout << (char)(a+b);
+}
