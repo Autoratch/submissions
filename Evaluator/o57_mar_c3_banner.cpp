88c9be5f05f0bafc3e0a302d595bebd98a009dbb
Autoratch <autorockza@gmail.com>
2020-06-07 09:01:01 +0700

    DP

--- Evaluator/o57_mar_c3_banner.cpp
+++ Evaluator/o57_mar_c3_banner.cpp
@@ -4,20 +4,19 @@ using namespace std;
 const int N = 301;
 
 int n,m;
-vector<int> s[N];
-int dp[N][N];
+int a[N][N],dp[N][N];
 bool visited[N][N];
 
 int solve(int l,int r)
 {
     if(visited[l][r]) return dp[l][r];
     visited[l][r] = true;
-    auto itl = lower_bound(s[l].begin(),s[l].end(),r);
-    auto itr = upper_bound(s[l].begin(),s[l].end(),r);
-    dp[l][r] = itr-itl;
-    int tmp = 0;
-    for(int i = l;i < r;i++) tmp = max(tmp,solve(l,i)+solve(i+1,r));
-    dp[l][r]+=tmp;
+    if(l==r) return dp[l][r] = a[l][r];
+    for(int i = l;i < r;i++)
+    {
+        dp[l][r] = max(dp[l][r],solve(l,i)+solve(i+1,r));   
+    }
+    dp[l][r]+=a[l][r];
     return dp[l][r];
 }
 
@@ -26,14 +25,11 @@ int main()
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     cin >> n >> m;
-
     for(int i = 0;i < m;i++)
     {
-        int a,b;
-        cin >> a >> b;
-        s[a].push_back(b);
+        int x,y;
+        cin >> x >> y;
+        a[x][y]++;
     }
-    for(int i = 1;i <= n;i++) sort(s[i].begin(),s[i].end());
-
     cout << m-solve(1,n);
 }
